f810f35e4e819cc1bafbac7cb18913754d5426d7
JP Quicksall <john.paul.quicksall@gmail.com>
2016-07-26 15:36:23 -0700

    Working prototype to recieve api call

---
+++ methods.py
@@ -0,0 +1,13 @@
+import os
+import requests
+import boto3
+import json
+
+from botocore.client import Config
+
+def getFunction(bucket_name, file_name):
+  return "You got {0} from the {1} bucket!".format(file_name, bucket_name)
+
+def postFunction(bucket_name, file_name, payload):
+  print payload
+  return "You posted {0} to the {1} bucket!".format(file_name, bucket_name)  
---
+++ routes.py
@@ -0,0 +1,27 @@
+import methods
+import os
+from flask import Flask, request, jsonify 
+app = Flask(__name__)
+
+port = 8080
+
+app.debug = True
+
+@app.route("/")
+def hello():
+  return "Katelin Rocks!"
+
+@app.route("/<bucket_name>/<filename>/", methods=['GET', 'POST'])
+def function1(bucket_name, filename):
+  if request.method == 'GET':
+    return methods.getFunction(bucket_name, filename)
+  elif request.method == 'POST':
+    payload = request.get_json()
+    return methods.postFunction(bucket_name, filename, payload)
+  else: 
+    return "Invalid"
+    
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=port)
